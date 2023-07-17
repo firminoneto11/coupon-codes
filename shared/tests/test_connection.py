fd08a16bbd455e0c13f2fcdf670bb72d70dfd2db
firminoneto11 <firminoneto11@hotmail.com>
2023-07-16 22:40:39 -0300

    test: added a test for the new ping method

--- shared/tests/test_connection.py
+++ shared/tests/test_connection.py
@@ -1,8 +1,8 @@
-from unittest.mock import AsyncMock, MagicMock
+from unittest.mock import AsyncMock, MagicMock, patch
 
 from pytest import raises
 
-from shared.connection import DBConnectionHandler
+from shared.connection import DBConnectionHandler, conn
 
 
 async def test_connection_handler() -> None:
@@ -26,3 +26,12 @@ async def test_connection_handler() -> None:
 
     assert mock_session.close.call_count == 1
     assert mock_session.close.await_count == 1
+
+
+async def test_ping_should_raise_exception() -> None:
+    expected_error_message = "Failed to connect to the database."
+    mock = MagicMock(side_effect=RuntimeError("A runtime error"))
+    with patch(target="shared.connection.text", new=mock), raises(Exception) as exc_info:
+        await conn.ping()
+
+    assert str(exc_info.value) == expected_error_message
